c0a17b81c47fd7a52228731e655a27dc0319e2a4
jnoconor <joe.oconor@gmail.com>
2013-10-12 17:53:14 -0400

    pre build commit

---
+++ lib/spdeck-scrape/spdeck-scrape.rb
@@ -0,0 +1,28 @@
+require_relative './spdeck-scraper-class.rb'
+
+# this file is loaded when 'spdeck-scrape' is required in a script
+
+# test code
+
+scraper = SpeakerdeckScraper.new("https://speakerdeck.com/", "ruby")
+scraper.query_results_scrape(3)
+scraper.scrape_all
+File.open('spd-ruby-raw', 'w') do |file|
+    file.write(scraper.presentations)
+end
+
+
+scraper.html_gen
+
+scraper2 = SpeakerdeckScraper.new("https://speakerdeck.com/", "json")
+scraper2.query_results_scrape(2)
+scraper2.scrape_all
+File.open('spd-json-raw', 'w') do |file|
+    file.write(scraper.presentations)
+end
+
+scraper2.html_gen
+
+system("open spd-ruby.html spd-json.html")
+
+# initialize a scraper with a website and a query
